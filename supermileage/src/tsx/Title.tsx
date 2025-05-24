43b31f2a4fe275743fc9306f85ec4a42a66c171b
YikaiFang <Yikai.fang1@gmail.com>
2025-05-24 11:11:49 -0700

    refactored some code top make code cleaner

---
+++ supermileage/src/tsx/Title.tsx
@@ -0,0 +1,20 @@
+import { JSX } from "react";
+
+type TitleProps = {
+    imageSrcPath: string;
+    name: string;
+};
+
+export default function Title({ imageSrcPath, name }: TitleProps): JSX.Element {
+    return (
+        <header
+            className="relative h-screen flex flex-col items-center justify-center text-center bg-cover bg-center"
+            style={{ backgroundImage: `url(${imageSrcPath})` }}
+        >
+            <div className="absolute inset-0 bottom-50 bg-gradient-to-b from-black/100 via-black/45 to-transparent"></div>
+            <h1 className="relative z-10 text-6xl font-bold text-white drop-shadow-lg">
+                {name}
+            </h1>
+        </header>
+    );
+}
