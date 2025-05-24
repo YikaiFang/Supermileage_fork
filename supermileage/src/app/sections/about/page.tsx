93deb68abaf67770b0243d025515eff810a93106
YikaiFang <Yikai.fang1@gmail.com>
2025-05-24 11:16:43 -0700

    about section refactored

--- supermileage/src/app/sections/about/page.tsx
+++ supermileage/src/app/sections/about/page.tsx
@@ -8,20 +8,7 @@ const About = () => {
   return (
     <div>
       <Header/>
-      <section>
-      <header
-        className="relative h-screen flex flex-col items-center justify-center text-center bg-cover bg-center"
-        style={{ backgroundImage: "url('/media/images/img_20190403_180711.jpg')" }}
-      >
-        <div className="absolute inset-0 bottom-50 bg-gradient-to-b from-black/100 via-black/45 to-transparent"></div>
-        <h1 className="relative z-10 text-6xl font-bold text-white drop-shadow-lg">
-          HISTORY
-        </h1>
-        <p className="relative z-10 text-xl italic text-white drop-shadow-md">
-          in the pursuit of efficiency
-        </p>
-      </header>
-      </section>
+      <Title imageSrcPath="/media/images/img_20190403_180711.jpg" name="History"/>
       <Timeline/>
       <Footer/>
     </div>
